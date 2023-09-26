c7af689aca1afd9859faa3bc72abbb1c9a96fc5e
tidzi17 <tidzi15110@gmail.com>
2023-09-26 19:47:41 +0200

    adding sections

--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {ImHome} from 'react-icons/im';
 import {BsCalendarCheck} from 'react-icons/bs';
 import '../../styles/main.scss';
+import { Link } from "react-router-dom";
 
 function Home(){
     return(
@@ -24,7 +25,30 @@ function Home(){
                 </div>
               </div>
             </div>
-            <div>else</div>
+            <section className="intro-container">
+            <div class="intro-container-text">
+                <h3>The building</h3>
+                <h2>Elevate your living experience</h2>
+                <p>Discover Harmony Residence in Miami, where elegant living harmonizes
+                    with stunning ocean vistas. Architect Luke Danes has crafted these
+                    residences to create an idyllic coastal experience.
+                </p>
+            </div>
+            <div class="intro-container-links">
+                <Link to={"./gallery"} class="intro-item intro-exterior">
+                    <h4>Exterior</h4>
+                    <h3>Costal Elegance In Harmony</h3>
+                </Link>
+                <Link to={"./neighbourhood"}  class="intro-item intro-neigh">
+                    <h4>Neighbourhood</h4>
+                    <h3>Harmonious Neighborhood Haven</h3>
+                </Link>
+                <Link to={"./gallery"}  class="intro-item intro-interior">
+                    <h4>Interior</h4>
+                    <h3>Timeless Interior Elegance</h3>
+                </Link>
+            </div>
+            </section>
         </div>
     )
 }
